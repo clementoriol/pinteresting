2da4257e9746ef6ef8db28efc56e8ce111cd7642
Clément Oriol <clement@clementoriol.com>
2016-03-18 13:44:14 +0100

    add password validation regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :password, :on => :create,
+                       :if => :password,
+                       :format => {/\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A=Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 end
